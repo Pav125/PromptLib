abf682ad579299b18f23ba1a8d637a1542631453
Pav125 <devipavan824@gmail.com>
2024-08-02 23:37:01 +0530

    added cor origin

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT
 const app = express()
 
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173','https://prompt-lib-nu.vercel.app'],
     methods: ['GET', 'POST'],
     credentials: true
 }))
