ad51ccdd04457cc9b9deb4230af73bab7a6cbc9a
Pav125 <devipavan824@gmail.com>
2024-08-03 00:20:11 +0530

    Added styling

--- frontend/src/components/Create.jsx
+++ frontend/src/components/Create.jsx
@@ -89,7 +89,7 @@ const Create = () => {
                 onClick={handleAddTag}
                 className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 focus:outline-none"
               >
-                Add Tag
+                Add
               </button>
             </div>
             <div className="flex flex-wrap mt-2">
